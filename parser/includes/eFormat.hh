5500c77932ccd0e434581381b885fe2fed3ebdf8
louazej <jean-baptiste.louazel@epitech.eu>
2013-05-19 17:42:38 +0200

    Ajout Parser

---
+++ parser/includes/eFormat.hh
@@ -0,0 +1,9 @@
+#ifndef	EFORMAT_HH__
+#define	EFORMAT_HH__
+
+enum eFormat {
+  XML,
+  JSON
+};
+
+#endif
---
+++ parser/sources/parser.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+#include <algorithm>
+#include <utility>
+
+#include "IDataFormat.hh"
+#include "eFormat.hh"
+#include "Parser.hh"
+#include "Error.hh"
+#include "Xml.hh"
+
+void	aff(std::pair<int, std::string const &> paire) {
+  std::cout << "line " << paire.first << "\t: " << paire.second << std::endl;
+}
+
+int	main(int ac, char **av)
+{
+  try {
+    if (ac < 2) {
+      std::cerr << "Usage is : ./parser file" << std::endl;
+      return -1;
+    }
+    IDataFormat *f = new Xml(av[1]);
+    //std::for_each(f->getContent().begin(), f->getContent().end(), aff);
+    delete f;
+    return 0;
+  }
+  catch (Error e) {
+      std::cerr << "Error of type \"" << e.getWhat() << "\" in " << e.getWhere() << " (" << e.getDetails() << ")" << std::endl;
+    }
+}
+
+
+
+
+
+
+
