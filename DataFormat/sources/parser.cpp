a01e78d1b81c549df0423d9f6d3da027f2290856
manour_m <manour_m@Manyuuz.local>
2013-05-20 17:19:21 +0200

    tentative

--- DataFormat/sources/parser.cpp
+++ DataFormat/sources/parser.cpp
@@ -1,40 +1,92 @@
 #include <map>
 #include <vector>
 #include <string>
-
 #include <algorithm>
-#include <utility>
-
+#include "Parser.hh"
 #include "IDataFormat.hh"
 #include "eFormat.hh"
-#include "Parser.hh"
 #include "Error.hh"
 #include "Xml.hh"
 
-void	aff(std::pair<int, std::string const &> paire) {
-  std::cout << "line " << paire.first << "\t: " << paire.second << std::endl;
-}
-
-int	main(int ac, char **av)
-{
-  try {
-    if (ac < 2) {
-      std::cerr << "Usage is : ./parser file" << std::endl;
-      return -1;
-    }
-    IDataFormat *f = new Xml(av[1]);
-    f->generate("pute.xml");
-    delete f;
-    return 0;
+std::string const Parser::typeToString(eFormat format) {
+  switch (format) {
+  case XML:
+    return "xml";
+  case JSON:
+    return "json";
+  default:
+    return "unknown";
   }
-  catch (Error e) {
-      std::cerr << "Error of type \"" << e.getWhat() << "\" in " << e.getWhere() << " (" << e.getDetails() << ")" << std::endl;
-    }
+  return 0;
 }
 
+bool	Parser::checkExtend(eFormat format, std::string const & filename) {
+  std::vector<std::string> trunc = splitString(filename, ".");
+  if (trunc.back() == typeToString(format))
+    return true;
+  return false;
+}
 
+IDataFormat *Parser::getAppropriateContainer(std::string const &filename) {
+  std::vector<std::string>  parsedName = splitString(filename, ".");
+  if (parsedName.back() == "xml")
+    return new Xml(filename);
+  throw (Error("invalid format", "parser", "the format \"" + parsedName.back() + "\" is not managed by the parser"));
+  return 0;
+}
 
+std::vector<std::string>	Parser::splitString(std::string const &s, std::string const &delim) {
+  std::vector<std::string>	vec;
+  std::string			str = s, tmp;
+  size_t			i = 0;
 
+  while (str.size() > 0 && (i = str.find_first_of(delim)) != std::string::npos)
+    {
+      tmp = str.substr(0, i);
+      if (tmp.size())
+	vec.push_back(tmp);
+      str = str.substr(i + 1, str.size());
+    }
+  tmp = str;
+  if (tmp.size())
+    vec.push_back(str);
+  return vec;
+}
 
+static bool ifEnd(std::string const & str, unsigned int i) {
+  for (; i < str.length(); i++) {
+    switch (str[i]) {
+    case ' ':
+      break;
+    case '\t':
+      break;
+    default:
+      return false;
+    }
+  }
+  return true;
+}
 
-
+std::string const Parser::epurString(std::string const & str) {
+  bool car = false;
+  std::string ret;
+  for (unsigned int i = 0; i < str.length(); i++) {
+    switch (str[i]) {
+    case ' ':
+      if (ifEnd(str, i) == true ||car == false)
+	break;
+      ret += str[i];
+      break;
+    case '\t':
+      if (ifEnd(str, i) == true || car == false)
+	break;
+      ret += str[i];
+      break;
+    default:
+      car = true;
+      ret += str[i];
+      break;
+    }
+  }
+  return ret;
+}
