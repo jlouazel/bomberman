67a02e3c9e9520b6eb47118f37144109b0a2d233
moreaup <alexandre.moreau@epitech.eu>
2013-05-30 21:07:48 +0200

    Tu vas de la merde happy encore

--- src/Display/Game.cpp
+++ src/Display/Game.cpp
@@ -57,7 +57,7 @@ namespace BomberMan
 	  firstMenu->addOnglet(newOnglet);
 	  this->menu_ = *firstMenu;
 	  this->menu_.initialize();
-	  this->addObject3d("libgdl/assets/marvin.fbx", vectorPosition, vectorRotation, vectorLen);
+	  this->addObject3d("texture/ExplodingBomb.fbx", vectorPosition, vectorRotation, vectorLen);
 	  std::list<AObject*>::iterator itb = this->objects_.begin();
 	  for (; itb != this->objects_.end(); ++itb)
 	    (*itb)->initialize();
@@ -110,7 +110,7 @@ namespace BomberMan
 	  std::list<AObject*>::iterator itb = this->objects_.begin();
 	  for (; itb != this->objects_.end(); ++itb)
 	    (*itb)->draw();
-	  this->menu_.affAllOnglet();
+	  // this->menu_.affAllOnglet();
         }
 
         void	MyGame::unload(void)
