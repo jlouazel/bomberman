8e1ceb7853767a59f646358bea68049fc7b0c1c4
Julien Fortin <julien.fortin@epitech.eu>
2013-06-02 00:08:37 +0200

    Adding core

---
+++ src/Core/BomberGame.cpp
@@ -0,0 +1,25 @@
+//
+// BomberGame.cpp for bomber in /home/fortin_j/tek2/projects/bomberman
+//
+// Made by julien fortin
+// Login   <fortin_j@epitech.net>
+//
+// Started on  Sat Jun  1 02:50:41 2013 julien fortin
+// Last update Sat Jun  1 23:33:17 2013 julien fortin
+//
+
+#include	"BomberGame.hh"
+
+namespace BomberMan
+{
+  namespace Core
+  {
+    BomberGame::BomberGame()
+    {
+    }
+
+    BomberGame::~BomberGame()
+    {
+    }
+  }
+}
