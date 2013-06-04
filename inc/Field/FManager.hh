29140739b1b568430be29313ca7bfa0154f1ca79
moreaup <alexandre.moreau@epitech.eu>
2013-06-04 12:47:47 +0200

    pjfdsis

--- inc/Field/FManager.hh
+++ inc/Field/FManager.hh
@@ -7,7 +7,6 @@
 #include <list>
 
 #include "IGameComponent.hh"
-#include "Camera.hpp"
 
 namespace BomberMan
 {
@@ -15,30 +14,18 @@ namespace BomberMan
     {
         class Manager
         {
-<<<<<<< HEAD
-	  unsigned int							_width;
-	  unsigned int							_height;
-	  std::vector<std::list<IGameComponent *> >				_map;
-	  Display::Camera							*_camera;
-
-
-	  Manager(const Manager &);
-	  Manager &	operator=(const Manager &);
-
-=======
             unsigned int	_width;
             unsigned int	_height;
-            
+
             std::vector<std::list<IGameComponent *> >	_map;
-                        
+
             Manager(const Manager &);
             Manager &	operator=(const Manager &);
-            
->>>>>>> 90c3fd04f4bb1d55a2fe62dcb4d9d1a2881981c8
+
         public:
-	  Manager(unsigned int width, unsigned int height);
+            Manager(unsigned int width, unsigned int height);
             ~Manager();
-            
+
             std::list<IGameComponent *> &	get(unsigned int, unsigned int);
             unsigned int				       getWidth() const;
             unsigned int					getHeight() const;
