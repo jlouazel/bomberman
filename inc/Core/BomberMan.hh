80be65ea477c880edda006e1b8376bc30c02ef7c
Manyuuz <M.Manoury@gmail.com>
2013-06-04 02:46:03 +0200

    test

--- inc/Core/BomberMan.hh
+++ inc/Core/BomberMan.hh
@@ -17,68 +17,57 @@
 
 namespace BomberMan
 {
-  namespace Core
-  {
-    const int	BOMBER_WIDTH	= 1920;
-    const int	BOMBER_HEIGHT	= 1080;
-
-    class BomberMan : public gdl::Game
+    namespace Core
     {
-    private:
-      // BomberMan();
-      // BomberMan(BomberMan const&);
-      BomberMan	&operator=(BomberMan const&);
+        const int	BOMBER_WIDTH	= 1920;
+        const int	BOMBER_HEIGHT	= 1080;
+        
+        class BomberMan : public gdl::Game
+        {
+        private:
+            bool	_intro;
+            bool	_menu;
+            bool	_game;
+            
+            Display::Camera	_camera;
+            BomberGame*		_currentGame;
+            
+            //std::map<IPlayer*, IController*>	_playerController;
 
-    private:
-      static BomberMan*	_bomberMan;
-      Display::Camera                camera_;
-
-    private:
-      bool	_intro;
-      bool	_menu;
-      bool	_game;
-
-    private:
-      BomberGame*		_currentGame;
-
-      //std::map<IPlayer*, IController*>	_playerController;
-
-    private:
-      void	_updateIntro();
-      void	_updateMenu();
-      void	_updateGame();
-
-      void	_initializeWindow();
-      void	_initializeResources() const;
-      void	_initializeIntro();
-      void	_initializeInput() const;
-      void	_initializeEvent() const;
-      void	_initializeMenu() const;
-
-      void	_drawIntro() const;
-      void	_drawMenu() const;
-      void	_drawGame() const;
-
-    protected:
-    public:
-      virtual ~BomberMan();
-
-      virtual void	initialize(void);
-      virtual void	update(void);
-      virtual void	unload(void);
-      virtual void	draw(void);
-
-      static BomberMan*	getBomberMan();
-      static void	deleteBomberMan();
-    };
-  }
+            BomberMan(BomberMan const&);
+            BomberMan &	operator=(BomberMan const&);
+            
+        public:
+            BomberMan();
+            virtual ~BomberMan();
+            
+            void	_updateIntro();
+            void	_updateMenu();
+            void	_updateGame();
+            
+            void	_initializeWindow();
+            void	_initializeResources() const;
+            void	_initializeIntro();
+            void	_initializeInput() const;
+            void	_initializeEvent() const;
+            void	_initializeMenu() const;
+            
+            void	_drawIntro() const;
+            void	_drawMenu() const;
+            void	_drawGame() const;
+            
+            virtual void	update(void);
+            virtual void	unload(void);
+            virtual void	draw(void);
+        };
+    }
 }
 #else
 namespace BomberMan
 {
-  namespace Core
-  {
-    class BomberMan;
-  }
+    namespace Core
+    {
+        class BomberMan;
+    }
 }
 #endif
--- src/Core/BomberMan.cpp
+++ src/Core/BomberMan.cpp
@@ -13,193 +13,169 @@
 
 namespace BomberMan
 {
-  namespace Core
-  {
-    BomberMan*	BomberMan::_bomberMan = 0;
-
-    BomberMan*	BomberMan::getBomberMan()
+    namespace Core
     {
-      if (!BomberMan::_bomberMan)
-	BomberMan::_bomberMan = new BomberMan;
-      if (!BomberMan::_bomberMan)
-	throw EndOfBomberMan("BomberMan", "getBomberMan", "NULL pointer");
-      return BomberMan::_bomberMan;
-    }
-
-    void		BomberMan::deleteBomberMan()
-    {
-      delete BomberMan::_bomberMan;
-      BomberMan::_bomberMan = 0;
-    }
-
-    // BomberMan::BomberMan()
-    // {
-    //   this->_intro = false;
-    //   this->_menu = false;
-    //   this->_game = true;
-
-    //   this->_currentGame = new BomberGame;
-    // }
-
-    BomberMan::~BomberMan()
-    {
-    }
-
-    void	BomberMan::initialize(void)
-    {
-      std::cout << "Je passe dans l'initialisation :(" << std::endl;
-      this->_intro = false;
-      this->_menu = false;
-      this->_game = true;
-
-      this->_currentGame = new BomberGame;
-
-      this->_initializeWindow();
-      this->_initializeIntro();
-      this->_initializeResources();
-      this->_initializeInput();
-      this->_initializeEvent();
-      this->_initializeMenu();
-
-      /* Initialisation ressources GDL */
-      /* Initialise Display */
-    }
-
-    void	BomberMan::_initializeWindow()
-    {
-      this->window_.setTitle("Breaking Bad");
-      this->window_.setHeight(BOMBER_HEIGHT);
-      this->window_.setWidth(BOMBER_WIDTH);
-      this->window_.create();
-      this->camera_.initialize();
-    }
-
-    void	BomberMan::_initializeResources() const
-    {
-      Display::Resources::getResources();
-    }
-
-    void	BomberMan::_initializeIntro()
-    {
-      this->_intro = false; /* false */
-    }
-
-    void	BomberMan::_initializeInput() const
-    {
-      Input::InputManager::getInputManager()->init();
-    }
-
-    void	BomberMan::_initializeEvent() const
-    {
-      Event::EventManager::getEventManager();
-    }
-
-    void	BomberMan::_initializeMenu() const
-    {
-      Display::MenuManager::getMenuManager();
-    }
-
-    void	BomberMan::update(void)
-    {
-      Input::Controller::KeyBoardManager::getKeyBoardManager()->treatInput(this->input_);
-      if (this->_intro)
-	this->_updateIntro();
-      else
-	{
-	  if (this->_menu)
-	    this->_updateMenu();
-	  else if (this->_game)
-	    this->_updateGame();
-	}
-    }
-
-    void	BomberMan::_updateIntro()
-    {
-      std::cout << "UPDATE INTRO\n";
-      this->_intro = false;
-      this->_menu = false; /* true */
-      Display::MenuManager::getMenuManager()->menu(Display::Menu::MAIN);
-    }
-
-    void	BomberMan::_updateMenu()
-    {
-      std::cout << "UPDATE MENU\n";
-      Display::MenuManager::getMenuManager()->update();
-    }
-
-    void	BomberMan::_updateGame()
-    {
-      if (this->_currentGame)
+        BomberMan::BomberMan()
         {
-	  // this->_currentGame->update();
-	  this->_currentGame->getManager()->Field::Manager::get(0, 0).front()->getAsset()->update(gameClock_);
-	}
+            // valeur par defaut : BomberMan inactif
+            this->_intro = false;
+            this->_menu = false;
+            this->_game = false;
+            this->_currentGame = 0;
+            
+            // initialisation : 1ere sequence = intro + load assets / ressources
+            this->_initializeWindow();
+            this->_initializeIntro();
+            this->_initializeResources();
+            this->_initializeInput();
+            this->_initializeEvent();
+            this->_initializeMenu();
+            
+            /* Initialisation ressources GDL */
+            /* Initialise Display */
+        }
+        
+        BomberMan::~BomberMan()
+        {
+            if (this->_currentGame)
+                delete this->_currentGame;
+        }
+        
+        void	BomberMan::_initializeWindow()
+        {
+            this->window_.setTitle("Breaking Bad");
+            this->window_.setHeight(BOMBER_HEIGHT);
+            this->window_.setWidth(BOMBER_WIDTH);
+            this->window_.create();
+            this->camera_.initialize();
+        }
+        
+        void	BomberMan::_initializeResources() const
+        {
+            Display::Resources::getResources();
+        }
+        
+        void	BomberMan::_initializeIntro()
+        {
+            this->_intro = true;
+        }
+        
+        void	BomberMan::_initializeInput() const
+        {
+            Input::InputManager::getInputManager()->init();
+        }
+        
+        void	BomberMan::_initializeEvent() const
+        {
+            Event::EventManager::getEventManager();
+        }
+        
+        void	BomberMan::_initializeMenu() const
+        {
+            Display::MenuManager::getMenuManager();
+        }
+        
+        void	BomberMan::update(void)
+        {
+            Input::Controller::KeyBoardManager::getKeyBoardManager()->treatInput(this->input_);
+            if (this->_intro)
+                this->_updateIntro();
+            else
+            {
+                if (this->_menu)
+                    this->_updateMenu();
+                else if (this->_game)
+                    this->_updateGame();
+            }
+        }
+        
+        void	BomberMan::_updateIntro()
+        {
+            std::cout << "UPDATE INTRO\n";
+            this->_intro = true;
+            this->_menu = false; /* true */
+            Display::MenuManager::getMenuManager()->menu(Display::Menu::MAIN);
+        }
+        
+        void	BomberMan::_updateMenu()
+        {
+            std::cout << "UPDATE MENU\n";
+            Display::MenuManager::getMenuManager()->update();
+        }
+        
+        void	BomberMan::_updateGame()
+        {
+            if (this->_currentGame)
+            {
+                // this->_currentGame->update();
+                this->_currentGame->getManager()->Field::Manager::get(0, 0).front()->getAsset()->update(gameClock_);
+            }
+        }
+        
+        void	BomberMan::draw(void)
+        {
+            camera_.update(gameClock_, input_);
+            if (this->_intro)
+                this->_drawIntro();
+            else
+            {
+                if (this->_menu || this->_game)
+                {
+                    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                    glClearColor(0.74f, 0.84f, 95.0f, 1.0f);
+                    glClearDepth(1.0f);
+                    glMatrixMode(GL_MODELVIEW);
+                }
+                if (this->_menu)
+                    this->_drawMenu();
+                else if (this->_game)
+                    this->_drawGame();
+            }
+        }
+        
+        void	BomberMan::_drawIntro() const
+        {
+        }
+        
+        void	BomberMan::_drawMenu() const
+        {
+            Display::MenuManager::getMenuManager()->draw();
+        }
+        
+        static void	affObjs(Field::IGameComponent * comp)
+        {
+            if (dynamic_cast<Field::Empty *>(comp) == comp)
+            {
+                comp->getAsset()->draw();
+                std::cout << "Empty (" << comp->getX() << ";" << comp->getY() << ")" << std::endl;
+                std::cout << "-------> " << comp->getAsset()->getPosition().getX() << std::endl;
+                std::cout << "-------> " << comp->getAsset()->getPosition().getY() << std::endl;
+                std::cout << "-------> " << comp->getAsset()->getPosition().getZ() << std::endl;
+            }
+        }
+        
+        void	BomberMan::_drawGame() const
+        {
+            // std::cout << "--START DRAW GAME--" << std::endl;
+            // for (unsigned int y = 0; y != this->_currentGame->getManager()->Field::Manager::getHeight(); y++)
+            // 	for (unsigned int x = 0; x != this->_currentGame->getManager()->Field::Manager::getWidth(); x++)
+            // 	  std::for_each(this->_currentGame->getManager()->Field::Manager::get(x, y).begin(), this->_currentGame->getManager()->Field::Manager::get(x, y).end(), affObjs);
+            this->_currentGame->getManager()->Field::Manager::get(0, 0).front()->getAsset()->draw();
+            // std::cout << "--DRAW GAME--" << std::endl;
+            //this->_currentGame->draw();
+            
+            for (unsigned int y = 0; y != this->_currentGame->getManager()->Field::Manager::getHeight(); y++)
+                for (unsigned int x = 0; x != this->_currentGame->getManager()->Field::Manager::getWidth(); x++)
+                    std::for_each(this->_currentGame->getManager()->Field::Manager::get(x, y).begin(), this->_currentGame->getManager()->Field::Manager::get(x, y).end(), affObjs);
+            std::cout << "--DRAW GAME--" << std::endl;
+        }
+        
+        void	BomberMan::unload(void)
+        {
+            Input::InputManager::deleteInputManager();
+            Event::EventManager::deleteEventManager();
+            Display::MenuManager::deleteMenuManager();
+            Display::Resources::deleteResources();
+        }
     }
-
-    void	BomberMan::draw(void)
-    {
-      camera_.update(gameClock_, input_);
-      if (this->_intro)
-	this->_drawIntro();
-      else
-	{
-	  if (this->_menu || this->_game)
-	    {
-	      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	      glClearColor(0.74f, 0.84f, 95.0f, 1.0f);
-	      glClearDepth(1.0f);
-	      glMatrixMode(GL_MODELVIEW);
-	    }
-	  if (this->_menu)
-	    this->_drawMenu();
-	  else if (this->_game)
-	    this->_drawGame();
-	}
-    }
-
-    void	BomberMan::_drawIntro() const
-    {
-    }
-
-    void	BomberMan::_drawMenu() const
-    {
-      Display::MenuManager::getMenuManager()->draw();
-    }
-
-    static void	affObjs(Field::IGameComponent * comp)
-    {
-      if (dynamic_cast<Field::Empty *>(comp) == comp)
-	{
-	  comp->getAsset()->draw();
-	  std::cout << "Empty (" << comp->getX() << ";" << comp->getY() << ")" << std::endl;
-	  std::cout << "-------> " << comp->getAsset()->getPosition().getX() << std::endl;
-	  std::cout << "-------> " << comp->getAsset()->getPosition().getY() << std::endl;
-	  std::cout << "-------> " << comp->getAsset()->getPosition().getZ() << std::endl;
-	}
-    }
-
-    void	BomberMan::_drawGame() const
-    {
-      // std::cout << "--START DRAW GAME--" << std::endl;
-      // for (unsigned int y = 0; y != this->_currentGame->getManager()->Field::Manager::getHeight(); y++)
-      // 	for (unsigned int x = 0; x != this->_currentGame->getManager()->Field::Manager::getWidth(); x++)
-      // 	  std::for_each(this->_currentGame->getManager()->Field::Manager::get(x, y).begin(), this->_currentGame->getManager()->Field::Manager::get(x, y).end(), affObjs);
-      this->_currentGame->getManager()->Field::Manager::get(0, 0).front()->getAsset()->draw();
-      // std::cout << "--DRAW GAME--" << std::endl;
-      //this->_currentGame->draw();
-
-      for (unsigned int y = 0; y != this->_currentGame->getManager()->Field::Manager::getHeight(); y++)
-	for (unsigned int x = 0; x != this->_currentGame->getManager()->Field::Manager::getWidth(); x++)
-	  std::for_each(this->_currentGame->getManager()->Field::Manager::get(x, y).begin(), this->_currentGame->getManager()->Field::Manager::get(x, y).end(), affObjs);
-      std::cout << "--DRAW GAME--" << std::endl;
-    }
-
-    void	BomberMan::unload(void)
-    {
-      Input::InputManager::deleteInputManager();
-      Event::EventManager::deleteEventManager();
-      Display::MenuManager::deleteMenuManager();
-      Display::Resources::deleteResources();
-    }
-  }
 }
