cccf2afe7f1e9af2469135ca84a6e6d054ce2bcb
louazej <jean-baptiste.louazel@epitech.eu>
2013-06-05 16:17:41 +0200

    Stairs

--- src/Field/FManager.cpp
+++ src/Field/FManager.cpp
@@ -14,13 +14,6 @@ namespace BomberMan
 {
   namespace Field
   {
-    static bool between(unsigned int low, unsigned int high, unsigned int nb)
-    {
-      if (nb >= low && nb <= high)
-	return true;
-      return false;
-    }
-
     static std::string const	randomiseWall()
     {
       unsigned int r = rand() % 100;
@@ -40,16 +33,15 @@ namespace BomberMan
 
     static void addEmptyObject(std::list<IGameComponent *> & components, unsigned int width, unsigned int height, unsigned int elemCnt)
     {
-      // std::vector<std::string> textures;
-      // textures.push_back("models/Wall1.fbx");
-      // textures.push_back("models/Wall2.fbx");
-      // textures.push_back("models/Wall3.fbx");
-      // textures.push_back("models/Wall3bis1.fbx");
-      // textures.push_back("models/Wall3bis2.fbx");
       Display::Vector3f	vectorLen(0.0, 0.0, 0.0);
       Display::Vector3f	vectorRot(0.0, 0.0, 0.0);
       Display::Vector3f	vectorPosition((elemCnt / width) * 220, 0.0, (elemCnt % width) * 220);
       components.push_front(new Empty(elemCnt / width, elemCnt % width, 0, 0, 0));
+      if (elemCnt % width == 0 && elemCnt / width == height - 1)
+	{
+	  vectorRot.setY(270.0);
+	  components.push_front(new Empty(elemCnt / width, elemCnt % width, new Display::Texture3d("models/Stairs.fbx", vectorPosition, vectorRot, vectorLen), 0, 0));
+	}
       if (elemCnt / width == height - 1)
 	{
 	  vectorRot.setY(270.0);
@@ -75,8 +67,8 @@ namespace BomberMan
       for (; this->_width < 15 || this->_width > 100; this->_width = rand() % 100);
       for (; this->_height < 15 || this->_height > 100; this->_height = rand() % 100);
 
-      this->_width = 100;
-      this->_height = 100;
+      this->_width = 5;
+      this->_height = 5;
 
       this->_map = std::vector<std::list<IGameComponent *> >(this->_width * this->_height, std::list<IGameComponent *>());
       unsigned int elemCnt = 0;
