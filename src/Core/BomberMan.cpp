1fbfd75cd868646ff77f121f609883e1564e4c57
moreaup <alexandre.moreau@epitech.eu>
2013-06-05 15:35:55 +0200

    Prepare for map 100-100

--- src/Core/BomberMan.cpp
+++ src/Core/BomberMan.cpp
@@ -91,7 +91,7 @@ namespace BomberMan
       for (unsigned int y = 0; y != this->_currentGame->getManager()->Field::Manager::getHeight(); y++)
 	for (unsigned int x = 0; x != this->_currentGame->getManager()->Field::Manager::getWidth(); x++)
 	  for (std::list<Field::IGameComponent *>::iterator it = this->_currentGame->getManager()->Field::Manager::get(x, y).begin(); it != this->_currentGame->getManager()->Field::Manager::get(x, y).end(); ++it)
-	    if (x > this->_currentGame->getPlayers().front()->getX() - 10 && x < this->_currentGame->getPlayers().front()->getX() + 10 && y > this->_currentGame->getPlayers().front()->getY() - 10 && y < this->_currentGame->getPlayers().front()->getY() + 10)
+	    if (x > (this->_currentGame->getPlayers().front()->getX() / 220) - 10 && x < (this->_currentGame->getPlayers().front()->getX() / 220) + 10 && y > (this->_currentGame->getPlayers().front()->getY() / 220) - 10 && y < (this->_currentGame->getPlayers().front()->getY() / 220) + 10)
 	      affObjs(*it, gameClock_, input_);
       // for (std::list<Field::Player *>::iterator it2 = this->_currentGame->getPlayers().begin(); it2 != this->_currentGame->getPlayers().end(); ++it2)
       // 	{
